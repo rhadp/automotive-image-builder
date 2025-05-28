7c257d78815e7b4edf90ceb13ed0ddcf38969caa
Dana Orr <dorr@redhat.com>
2025-05-28 11:42:15 +0300

    Add integration test for enable_repos and repos configuration Fixes: https://gitlab.com/CentOS/automotive/src/automotive-image-builder/-/issues/42 Signed-off-by: Dana Orr <dorr@redhat.com>

--- tests/tests/install-rpms/test-install-rpms.sh
+++ tests/tests/install-rpms/test-install-rpms.sh
@@ -3,18 +3,31 @@
 source $(dirname $BASH_SOURCE)/../../scripts/test-lib.sh
 
 echo_log "Starting build..."
-build --export rpmlist test.aib.yml out.json
+build --distro cs9 --export rpmlist test.aib.yml out.json
 echo_log "Build completed, output: out.json"
 
+# Check if rootfs contains 'strace'
 echo_log "Checking if rootfs contains 'strace'..."
 cat out.json | jq '.rootfs|has("strace")' > has_strace.txt
-echo_log "Extracted strace check result to has_strace.txt"
 assert_file_has_content has_strace.txt true
-echo_log "Assertion completed for has_strace.txt"
+echo_log "Assertion completed for strace."
 
+# Check if rootfs contains 'coreutils-debuginfo'
+echo_log "Checking if rootfs contains 'coreutils-debuginfo'..."
+cat out.json | jq '.rootfs|has("coreutils-debuginfo")' > has_coreutils_debuginfo.txt
+assert_file_has_content has_coreutils_debuginfo.txt true
+echo_log "Assertion completed for coreutils-debuginfo."
+
+# Check if rootfs contains 'htop'
+echo_log "Checking if rootfs contains 'htop'..."
+cat out.json | jq '.rootfs|has("htop")' > has_htop.txt
+assert_file_has_content has_htop.txt true
+echo_log "Assertion completed for htop."
+
+# Check if qm_rootfs_base contains 'less'
 echo_log "Checking if qm_rootfs_base contains 'less'..."
 cat out.json | jq '.qm_rootfs_base|has("less")' > qm_has_less.txt
-echo_log "Extracted less check result to qm_has_less.txt"
 assert_file_has_content qm_has_less.txt true
-echo_log "Assertion completed for qm_has_less.txt"
+echo_log "Assertion completed for less."
 
+echo_log "PASS: All package installation checks completed successfully."
