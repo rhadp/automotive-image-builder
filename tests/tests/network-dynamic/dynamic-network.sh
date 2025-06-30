07ff7640109723da8e00ab392442f01df177d3e1
nsimsolo <nsimsolo@redhat.com>
2025-06-30 16:37:53 +0300

    add aib-network-dynamic integration test
    Fixes: https://gitlab.com/CentOS/automotive/src/automotive-image-builder/-/issues/50
    
    Signed-off-by: nsimsolo <nsimsolo@redhat.com>

---
+++ tests/tests/network-dynamic/dynamic-network.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash -x
+
+source "$(dirname "$BASH_SOURCE")/../../scripts/test-lib.sh"
+
+build --export tar --extend-define "tar_paths=['etc','usr/lib']" test-network-dynamic.aib.yml out.tar
+tar xf out.tar
+
+# 1. Ensure no static network config files exist
+assert_not_has_file etc/main.nmstate
+assert_not_has_file usr/lib/boot-check.d/nmstate.conf
+
+# 2. Ensure no persistent NetworkManager connection profiles exist
+if ls etc/NetworkManager/system-connections/*.nmconnection &>/dev/null; then
+  echo_fail "persistent nmconnection found"
+  exit 1
+fi
+
+# 3. Ensure NetworkManager is enabled at boot
+nm_service="etc/systemd/system/multi-user.target.wants/NetworkManager.service"
+if [[ ! -L "$nm_service" || "$(readlink "$nm_service")" != "/usr/lib/systemd/system/NetworkManager.service" ]]; then
+  echo_fail "NetworkManager.service is not properly enabled"
+  exit 1
+fi
+
+echo_pass "dynamic networking configured correctly (no static profiles, NetworkManager enabled)"
+
