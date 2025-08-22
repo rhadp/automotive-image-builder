d7d49e096cf69026f1b291a90d0973ca257355b7
Martin Perina <mperina@redhat.com>
2025-08-22 14:37:01 +0200

    Rename selinux-booleans test to selinux-config
    Relates: https://gitlab.com/CentOS/automotive/src/automotive-image-builder/-/issues/80
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- tests/tests/selinux-config/selinux-config.sh
+++ tests/tests/selinux-config/selinux-config.sh
@@ -19,7 +19,7 @@ EXPECTED_SELINUX_BOOLEANS=(
 
 # Build the image
 echo_log "Building AIB image..."
-build --target qemu --mode image --export image test-selinux-booleans.aib.yml "$IMG_NAME"
+build --target qemu --mode image --export image test-selinux-config.aib.yml "$IMG_NAME"
 
 # Check if image was created
 assert_image_exists "$IMG_NAME"
