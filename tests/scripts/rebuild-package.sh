dc4713c4955cae4500b796b43ee5415309bd882e
Martin Perina <mperina@redhat.com>
2025-09-24 15:37:54 +0200

    Fix shellcheck issue SC2027
    Fixes following shellcheck issue:
    
      SC2027 (warning): The surrounding quotes actually unquote this. Remove or escape them.
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- tests/scripts/rebuild-package.sh
+++ tests/scripts/rebuild-package.sh
@@ -11,12 +11,12 @@ if [ "${BUILD_AIB_RPM}" != "yes" ]; then
 fi
 
 if [ ! -d "${AIB_SRPM_DIR}" ]; then
-    echo "Directory '"${AIB_SRPM_DIR}"' does not exist"
+    echo "Directory '${AIB_SRPM_DIR}' does not exist"
     exit 1
 fi
 
 if [[ $(ls -1q ${AIB_SRPM_DIR}/*.src.rpm 2> /dev/null | wc -l) < 1 ]]; then
-    echo "Directory '"${AIB_SRPM_DIR}"' does not contain any source RPM packages"
+    echo "Directory '${AIB_SRPM_DIR}' does not contain any source RPM packages"
     exit 2
 fi
 
--- tests/scripts/setup-repos.sh
+++ tests/scripts/setup-repos.sh
@@ -9,7 +9,7 @@ dnf update -y
 add_repo "aib-base-repo" ${AIB_BASE_REPO}
 
 # Configure a-i-b custom repository when specified
-echo "CUSTOM_REPO='"${AIB_CUSTOM_REPO}"'"
+echo "CUSTOM_REPO='${AIB_CUSTOM_REPO}'"
 
 if [ -n "${AIB_CUSTOM_REPO}" ]; then
     add_repo "aib-custom-repo" ${AIB_CUSTOM_REPO}
